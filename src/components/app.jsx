08b82f189fcf0fa77ba733f77901078545476be5
holi0317 <holliswuhollis@gmail.com>
2017-07-02 22:09:32 +0800

    Scaffold application layout

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,3 +1,21 @@
-export function App() {
+import * as React from 'react'
+import {Webcam} from './webcam'
+import {Status} from './status'
+import {Log} from './log'
+import {NotifySettings} from './notify-settings'
 
+export function App() {
+  return (
+    <div>
+      <h1>JSSE PH23 smart mail box</h1>
+      <h3>Webcam footage</h3>
+      <Webcam />
+      <h3>Current status</h3>
+      <Status />
+      <h3>Mail Log</h3>
+      <Log />
+      <h3>Notification settings</h3>
+      <NotifySettings />
+    </div>
+  )
 }
---
+++ src/components/notify-settings.jsx
@@ -0,0 +1,3 @@
+export function NotifySettings() {
+  return null
+}
