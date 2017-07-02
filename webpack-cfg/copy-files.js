1f3b1180e4b35d4a8b1b8e21e55c0d434466fc2b
holi0317 <holliswuhollis@gmail.com>
2017-07-02 23:24:15 +0800

    Setup firebase push notification

--- webpack-cfg/copy-files.js
+++ webpack-cfg/copy-files.js
@@ -3,7 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   plugins: [
     new CopyWebpackPlugin([
-
+      {from: 'manifest.json', to: 'manifest.json'}
     ])
   ]
 }
