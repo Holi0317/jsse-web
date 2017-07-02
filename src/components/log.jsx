aeab8ac5d93252e6fad988c7258fb3521f8527ba
holi0317 <holliswuhollis@gmail.com>
2017-07-03 00:43:14 +0800

    Add log

--- src/components/log.jsx
+++ src/components/log.jsx
@@ -1,3 +1,38 @@
-export function Log() {
-  return null
+import firebase from 'firebase/app'
+import * as React from 'react'
+const database = firebase.database()
+
+const ref = database.ref('log')
+
+export class Log extends React.Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      logs: []
+    }
+  }
+
+  componentWillMount() {
+    ref.on('child_added', data => {
+      this.setState(state => {
+        const logs = state.logs.slice()
+        logs.push(data.val())
+        return {
+          logs
+        }
+      })
+    })
+  }
+
+  render() {
+    const {logs} = this.state
+    return (
+      <ul>
+        {logs.map(log => (
+          <li key={log}>{log}</li>
+        ))}
+      </ul>
+
+    )
+  }
 }
