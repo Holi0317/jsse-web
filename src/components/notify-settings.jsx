2c284065c8722b241c898942f3c49cc4b032bacc
holi0317 <holliswuhollis@gmail.com>
2017-07-02 23:24:34 +0800

    Minimal UI for push notification

--- src/components/notify-settings.jsx
+++ src/components/notify-settings.jsx
@@ -1,3 +1,37 @@
-export function NotifySettings() {
-  return null
+import * as React from 'react'
+
+function isSupported() {
+  return window.PushManager && navigator.serviceWorker && Notification
+}
+
+export class NotifySettings extends React.PureComponent {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      supported: isSupported(),
+      permission: Notification.permission
+    }
+  }
+
+  requestPermission = async () => {
+    const result = await Notification.requestPermission()
+    this.setState({
+      permission: result
+    })
+  }
+
+  render() {
+    const {supported, permission} = this.state
+    if (!supported) {
+      return <span>Push notification is not supported.</span>
+    }
+    if (permission === 'denied') {
+      return <span>Notification is denied. Check <a href="https://support.google.com/chrome/answer/3220216?co=GENIE.Platform%3DDesktop&hl=en">this</a> to enable it</span>
+    }
+    if (permission === 'default') {
+      return <button onClick={this.requestPermission}>Enable Notification</button>
+    }
+
+    return null
+  }
 }
