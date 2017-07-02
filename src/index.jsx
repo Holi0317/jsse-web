91cb55412e63237a7d0c8f825902eb9cb7e9852a
holi0317 <holliswuhollis@gmail.com>
2017-07-02 21:41:23 +0800

    Add index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore, compose, applyMiddleware} from 'redux'
+import {reducer} from './reducer/index'
+import {App} from './components/app'
+
+const middlewares = []
+
+if (process.env.NODE_ENV === 'development') {
+  const {createLogger} = require('redux-logger')
+  const logger = createLogger({
+    collapsed: true
+  })
+  middlewares.push(logger)
+}
+
+const store = compose(applyMiddleware(...middlewares))(createStore)(reducer)
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
+
+ReactDOM.render(<Root />, document.body)
