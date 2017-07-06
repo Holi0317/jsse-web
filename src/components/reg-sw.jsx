325ee44c3c53fda9decb365009c5dbf4251fda4d
holi0317 <holliswuhollis@gmail.com>
2017-07-06 21:35:05 +0800

    Remove service worker being put into redux store

--- src/components/reg-sw.jsx
+++ src/components/reg-sw.jsx
@@ -1,25 +1,12 @@
 import * as React from 'react'
 import * as firebase from 'firebase/app'
 import runtime from 'serviceworker-webpack-plugin/lib/runtime'
-import {connect} from 'react-redux'
 const messaging = firebase.messaging()
 
-const mapDispatchToProps = dispatch => ({
-  setSW(registration) {
-    dispatch({
-      type: 'SW/SET_SW',
-      sw: registration
-    })
-  }
-})
-
-@connect(null, mapDispatchToProps)
 export class RegSW extends React.Component {
   async componentWillMount() {
-    const {setSW} = this.props
     if ('serviceWorker' in navigator) {
       const registration = runtime.register()
-      setSW(registration)
       const reg = await registration
       messaging.useServiceWorker(reg)
     }
--- src/selectors/sw.js
+++
@@ -1,6 +0,0 @@
-/**
- * Select service worker registration promise from redux store.
- * Selected value may be null
- */
-export const sw = state =>
-  state.sw
