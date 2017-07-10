79019f506fe1fef40d1edb4e79ab4ca732403d1a
holi0317 <holliswuhollis@gmail.com>
2017-07-10 17:38:17 +0800

    Use ES module of react-redux-firebase

--- webpack-cfg/alias.js
+++ webpack-cfg/alias.js
@@ -3,13 +3,7 @@ module.exports = {
     alias: {
       // Use non-compiled version of the following libraries
       'reselect': 'reselect/src/index.js',
-
-      // Use non-compiled version of the following react libraries
-      'react-icon-base': 'react-icon-base/index.js',
-      'react-collapse': 'react-collapse/src/index.js',
-      'react-height': 'react-height/src/index.js',
-      'react-i18next': 'react-i18next/src/index.js',
-      'react-sortable-hoc': 'react-sortable-hoc/dist/es6'
+      'react-redux-firebase': 'react-redux-firebase/es'
     }
   }
 }
