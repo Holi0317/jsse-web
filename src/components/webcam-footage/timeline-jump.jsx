9de688c725ce510a7af7c111d9a20d0cae59361d
holi0317 <holliswuhollis@gmail.com>
2017-07-11 02:24:34 +0800

    Implement time setter logic

--- src/components/webcam-footage/timeline-jump.jsx
+++ src/components/webcam-footage/timeline-jump.jsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
+import moment from 'moment'
 import {connect} from 'react-redux'
 import {firebaseConnect} from 'react-redux-firebase'
 import {TimeSelector} from './time-selector'
+import {footageSelector} from '../../selectors/footages'
+import {tmpDateSelector} from '../../selectors/tmp-date'
+import {tmpHourSelector} from '../../selectors/tmp-hour'
 
 const mapStateToProps = state => ({
-
+  tmpDate: tmpDateSelector(state),
+  tmpHour: tmpHourSelector(state),
+  footages: footageSelector(state)
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -22,8 +28,18 @@ const mapDispatchToProps = dispatch => ({
 @connect(mapStateToProps, mapDispatchToProps)
 export class TimelineJump extends React.Component {
   changeTime = () => {
-    const {changeTime} = this.props
+    const {footages, tmpDate, tmpHour, changeTime} = this.props
 
+    if (!(tmpDate && tmpHour)) {
+      alert('You must specify both date and hour before jump timeline.') // Its blocking. But I have no time.
+      return
+    }
+
+    const momented = footages.map(footage => moment(footage.time))
+    const desiredTime = moment(`${tmpDate} ${tmpHour}`, 'YYYY-MM-DD HH')
+    const time = momented.find(time => time.isSame(desiredTime, 'hour'))
+
+    changeTime(time.valueOf())
   }
 
   render() {
---
+++ src/selectors/tmp-hour.js
@@ -0,0 +1,2 @@
+export const tmpHourSelector = state =>
+  state.footage.tmpHour || ''
