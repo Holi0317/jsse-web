7c2761345693f356d7450ac0bda777126840d200
holi0317 <holliswuhollis@gmail.com>
2017-07-11 03:00:38 +0800

    Get url from client instead from server

--- src/components/webcam-footage/footage-img.jsx
+++ src/components/webcam-footage/footage-img.jsx
@@ -3,9 +3,10 @@ import {connect} from 'react-redux'
 import {firebaseConnect} from 'react-redux-firebase'
 import {dispFootageImgSelector} from '../../selectors/disp-footage-img'
 import {dispFootageTimeSelector} from '../../selectors/disp-footage-time'
+import {FBStorageImage} from '../fb-storage-image'
 
 const mapStateToProps = state => ({
-  imgUrl: dispFootageImgSelector(state),
+  imgPath: dispFootageImgSelector(state),
   time: dispFootageTimeSelector(state)
 })
 
@@ -14,10 +15,11 @@ const mapStateToProps = state => ({
 ])
 @connect(mapStateToProps)
 export class FootageImg extends React.Component {
+
   render() {
-    const {imgUrl, time} = this.props
-    if (imgUrl && time) {
-      return <img height="480px" src={imgUrl} alt={time.calendar()} />
+    const {imgPath, time} = this.props
+    if (imgPath && time) {
+      return <FBStorageImage height="480px" path={imgPath} alt={time.calendar()} />
     }
     return null
   }
--- src/firebase-init.js
+++ src/firebase-init.js
@@ -2,6 +2,7 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 import 'firebase/messaging'
+import 'firebase/storage'
 
 export const config = {
   apiKey: 'AIzaSyABL_RzS1xsUMSpVMY18LI24-yHV6LETjQ',
