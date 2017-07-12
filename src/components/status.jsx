ca09e6e5a0575ec2579e4422fc9b2e4327a5d310
holi0317 <holliswuhollis@gmail.com>
2017-07-12 21:16:16 +0800

    Add mail count display and reset button

--- src/components/status.jsx
+++ src/components/status.jsx
@@ -2,21 +2,31 @@ import * as React from 'react'
 import {connect} from 'react-redux'
 import {firebaseConnect} from 'react-redux-firebase'
 import {statusSelector} from '../selectors/status'
+import {mailCountSelector} from '../selectors/mail-count'
 
 const mapStateToProps = state => ({
-  status: statusSelector(state)
+  status: statusSelector(state),
+  mailCount: mailCountSelector(state)
 })
 
 @firebaseConnect([
-  'status'
+  'status',
+  'mailCount'
 ])
 @connect(mapStateToProps)
 export class Status extends React.Component {
+  resetMail = () => {
+    const {firebase} = this.props
+    firebase.set('mailCount', 0)
+  }
+
   render() {
-    const {status} = this.props
+    const {status, mailCount} = this.props
     return (
       <div>
         <div>Status: {status}</div>
+        <div>Number of mail: {mailCount}</div>
+        <button onClick={this.resetMail}>Reset number of mail</button>
       </div>
     )
   }
---
+++ src/selectors/mail-count.js
@@ -0,0 +1,2 @@
+export const mailCountSelector = ({firebase}) =>
+  firebase.data.mailCount
