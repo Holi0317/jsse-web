4bb5c0dc0ee601277e375daa3477ae7906d5d415
holi0317 <holliswuhollis@gmail.com>
2017-07-12 19:58:14 +0800

    Fix broken source map in production

--- webpack-cfg/babili.js
+++ webpack-cfg/babili.js
@@ -1,10 +1,13 @@
 const Babili = require('babili-webpack-plugin')
+const {ENV} = require('./paths')
 
 module.exports = {
   plugins: [
     new Babili({
       removeConsole: false,
       removeDebugger: true
+    }, {
+      sourceMap: ENV === 'production' ? 'hidden-source-map' : 'inline-source-map',
     })
   ]
 }
